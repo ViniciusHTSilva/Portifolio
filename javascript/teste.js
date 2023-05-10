28f37dd6dea7c09689303949d3ead672025b4870
Vinicius <88394501+ViniciusHTSilva@users.noreply.github.com>
2023-05-10 00:17:21 -0300

    V 0.3
    atualização de videos

---
+++ javascript/teste.js
@@ -0,0 +1,12 @@
+var video = document.querySelector('#image-sobre');
+
+
+video.addEventListener('load', function(){  
+
+    if(form.style.display === 'none'){
+        form.style.display = 'block';
+    }else {
+        form.style.display = 'block';
+    }
+
+})
